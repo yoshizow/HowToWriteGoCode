b833e1fec18e0d7557d84f806901ea226e6f17ba
Yoshitaro Makise <yoshizow@turtlewalk.org>
2014-01-30 00:48:44 +0900

    Added library.

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/yoshizow/HowToWriteGoCode/newmath"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("hello, world.  Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
